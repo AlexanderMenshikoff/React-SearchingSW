080f35ea7bb04d1104ed312491d70105f117cbfd
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2024-01-21 18:15:05 +0500

    got data from starwarsapi, got input data, display heroes info

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
 import SearchingInput from "./components/SearchingInput";
+import { useState } from "react";
 
 const App = () => {
-  // const getHeroesList = async () => {
-  //   const heroesResp = await fetch("https://swapi.dev/api/people/", {
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
+  const [heroList, setHeroList] = useState([]);
+  const [inputValue, setInputValue] = useState();
 
-  //   const respDataHeroes = await heroesResp.json();
-  //   console.log(respDataHeroes);
-  // };
+  const getHeroesList = async () => {
+    const heroesResp = await fetch("https://swapi.dev/api/people/", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const respDataHeroes = await heroesResp.json();
+    console.log(respDataHeroes);
+    const result = respDataHeroes.results;
+    setHeroList(result);
+
+    // setHeroList(result);
+
+    // for (let index = 0; index < respDataHeroes.results.length; index++) {
+    //   const el = respDataHeroes.results[index];
+
+    //   const info = (
+    //     <div>
+    //       <p>{el.name}</p>
+    //       <p>{el.birth_year}</p>
+    //       <p>{el.gender}</p>
+    //       <p>{el.height}</p>
+    //       <p>{el.mass}</p>
+    //     </div>
+    //   );
+    //   setHeroList(info);
+    //   console.log(heroList);
+    // }
+  };
+
+  // console.log(heroList);
 
   return (
     <div className="App">
       <div className="flex justify-center m-[30px]">
-        <SearchingInput />
+        <SearchingInput
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          getHeroes={getHeroesList}
+        />
       </div>
+      {heroList.map((el, i) => {
+        return (
+          el.name.toLowerCase() === inputValue.toLowerCase() && (
+            <div key={i}>
+              <p>{el.name}</p>
+              <p>{el.birth_year}</p>
+              <p>{el.gender}</p>
+              <p>{el.height}</p>
+              <p>{el.mass}</p>
+            </div>
+          )
+        );
+      })}
     </div>
   );
 };
