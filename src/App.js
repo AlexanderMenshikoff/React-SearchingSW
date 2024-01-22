6cec65a561564950a06d2658c3273fe2564b04ba
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2024-01-22 17:54:48 +0500

    added useEffect, deleted useless code(comments), combined all heroes objs together(to search and display all variants), added input checking(if input is empty nothing is displaying)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
 import SearchingInput from "./components/SearchingInput";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const [heroList, setHeroList] = useState([]);
-  const [inputValue, setInputValue] = useState();
+  const [inputValue, setInputValue] = useState("");
 
   const getHeroesList = async () => {
-    const heroesResp = await fetch("https://swapi.dev/api/people/", {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
+    const heroesResp = await fetch("https://swapi.dev/api/people/");
     const respDataHeroes = await heroesResp.json();
-    console.log(respDataHeroes);
-    const result = respDataHeroes.results;
-    setHeroList(result);
-
-    // setHeroList(result);
-
-    // for (let index = 0; index < respDataHeroes.results.length; index++) {
-    //   const el = respDataHeroes.results[index];
-
-    //   const info = (
-    //     <div>
-    //       <p>{el.name}</p>
-    //       <p>{el.birth_year}</p>
-    //       <p>{el.gender}</p>
-    //       <p>{el.height}</p>
-    //       <p>{el.mass}</p>
-    //     </div>
-    //   );
-    //   setHeroList(info);
-    //   console.log(heroList);
-    // }
+    const arr = [];
+    for (let index = 1; index <= respDataHeroes.count; index++) {
+      const heroesResp2 = await fetch("https://swapi.dev/api/people/" + index);
+      const respDataHeroes2 = await heroesResp2.json();
+      arr.push(respDataHeroes2);
+    }
+    setHeroList(arr);
   };
 
-  // console.log(heroList);
+  useEffect(() => {
+    getHeroesList();
+  }, []);
 
   return (
     <div className="App">
@@ -47,19 +30,21 @@ const App = () => {
           getHeroes={getHeroesList}
         />
       </div>
-      {heroList.map((el, i) => {
-        return (
-          el.name.toLowerCase() === inputValue.toLowerCase() && (
-            <div key={i}>
-              <p>{el.name}</p>
-              <p>{el.birth_year}</p>
-              <p>{el.gender}</p>
-              <p>{el.height}</p>
-              <p>{el.mass}</p>
-            </div>
-          )
-        );
-      })}
+      {inputValue
+        ? heroList.map((el, i) => {
+            return (
+              el.name?.toLowerCase().includes(inputValue?.toLowerCase()) && (
+                <div key={i}>
+                  <p>{el.name}</p>
+                  <p>{el.birth_year}</p>
+                  <p>{el.gender}</p>
+                  <p>{el.height}</p>
+                  <p>{el.mass}</p>
+                </div>
+              )
+            );
+          })
+        : ""}
     </div>
   );
 };
