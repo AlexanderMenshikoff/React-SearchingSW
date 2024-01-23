ddf4cba0c37eabc59bc893293f46f3b970b94ed1
Alex_Menshikoff <menshikov.sasha2015@yandex.ru>
2024-01-23 20:29:30 +0500

    added visibility props, changed some colors and sizes

--- src/components/SearchingInput.js
+++ src/components/SearchingInput.js
@@ -6,15 +6,15 @@ const SearchingInput = (props) => {
   };
 
   return (
-    <div>
+    <div className={props.visibility}>
       <form onSubmit={handleSubmit}>
-        <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+        <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white ">
           Search
         </label>
         <div className="relative">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
             <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              className="w-4 h-4 text-gray-500  dark:text-gray-400"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -32,7 +32,7 @@ const SearchingInput = (props) => {
           <input
             type="search"
             id="search"
-            className="w-[500px] block p-4 ps-10 text-sm outline-none text-gray-900 border border-gray-300 rounded-[25px] bg-gray-50 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 search-cancel:fill-green-400 search-cancel:h-4 search-cancel:w-4 search-cancel:appearance-none search-cancel:bg-[url(https://pro.fontawesome.com/releases/v5.10.0/svgs/solid/times-circle.svg)] "
+            className="w-[25vw] block p-4 ps-10 text-sm outline-none text-gray-300  border border-gray-500 rounded-[25px] bg-black group focus:ring-gray-500 focus:border-yellow-400 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 search-cancel:fill-green-400 search-cancel:h-4 search-cancel:w-4 search-cancel:appearance-none search-cancel:bg-[url(https://pro.fontawesome.com/releases/v5.10.0/svgs/solid/times-circle.svg)]  "
             placeholder="Введите имя персонажа по-английски"
             required
             value={props.inputValue}
